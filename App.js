e84218adb56963f8f61dc6ba2ad8db26c3a4105f
Ginie Digital <giniedigitaldeveloper@gmail.com>
2025-10-27 22:46:43 +0530

    Initial setup with expo sdk 53 alongside Phase 1 library installation

---
+++ App.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Constants from 'expo-constants';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
