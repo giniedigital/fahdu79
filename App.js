b1230638305a542daaa53b5af50e896010eab5e7
Ginie Digital <giniedigitaldeveloper@gmail.com>
2025-10-28 14:09:18 +0530

    P1

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Constants from 'expo-constants';
 
 const App = () => {
+  console.log('fuck');
   return (
-    <View>
+    <SafeAreaView>
       <Text>Hello</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
